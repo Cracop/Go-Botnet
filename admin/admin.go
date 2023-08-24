50e79ff9c3d4e7d733e9137e65a62d6c9e7756f3
Cracop <rodrigoalejandrobarrera@gmail.com>
2023-08-24 09:30:02 -0600

    Started the admin client

--- admin/admin.go
+++ admin/admin.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/joho/godotenv"
 )
+
+var (
+	broadcast = make(chan string) // Channel for broadcasting messages
+	conn      net.Conn
+)
+
+func sendMessage() {
+	for {
+		message := <-broadcast
+		_, err := conn.Write([]byte(message)) //Sends message to clients
+		if err != nil {
+			fmt.Println("Error broadcasting data:", err)
+		}
+	}
+}
 
 func main() {
 	// Load variables from .env file
@@ -17,10 +36,60 @@ func main() {
 
 	// Access environment variables
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	//dbPort := os.Getenv("DB_PORT")
 
 	fmt.Println("Database Host:", dbHost)
-	fmt.Println("Database Port:", dbPort)
+	//fmt.Println("Database Port:", dbPort)
+
+	//Connect to the server
+	serverAddr := "127.0.0.1:8080"
+
+	/*
+		go func() {
+			for {
+				fmt.Print("GoBot> ")
+				reader := bufio.NewReader(os.Stdin)
+				command, _ := reader.ReadString('\n')
+				command = strings.TrimRight(command, "\n")
+
+				// Check if the user wants to exit
+				if command == "exit" {
+					fmt.Println("Exiting the client.")
+					break
+				}
+			}
+		}()
+	*/
+	go sendMessage()
+
+	for {
+
+		if conn == nil {
+
+			newConn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				fmt.Println("Error connecting to the server:", err)
+				time.Sleep(time.Second) // Wait before attempting to reconnect
+				continue
+			}
+			fmt.Println("Connected to the server")
+			conn = newConn
+		}
+		fmt.Print("GoBot> ")
+		reader := bufio.NewReader(os.Stdin)
+		command, _ := reader.ReadString('\n')
+		command = strings.TrimRight(command, "\n")
+
+		// Check if the user wants to exit
+		if command == "exit" {
+			fmt.Println("Exiting the Admin Panel.")
+			break
+		}
+
+		broadcast <- command
+
+	}
+
 	/*
 		serverAddr := "192.168.1.65:8080"
 		var conn net.Conn
